06f6a74924df8769a08a12c356fd71ccc8b9f5bb
Marcel Hasselaar <marcel@unbadapps.com>
2017-12-13 12:08:38 +0100

    A paged UIScrollView with a UIPageControl

--- PagedScrollView/ViewController.swift
+++ PagedScrollView/ViewController.swift
@@ -10,16 +10,88 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    private let portfolioWidthToContainingViewFactor = CGFloat(0.85)
+    private let portfolioHeightToContainingViewFactor = CGFloat(0.6)
+
+    fileprivate let scrollView = UIScrollView()
+    fileprivate let pageControl = UIPageControl()
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        pageControl.translatesAutoresizingMaskIntoConstraints = false
+        pageControl.pageIndicatorTintColor = .gray
+        pageControl.currentPageIndicatorTintColor = .blue
+        pageControl.numberOfPages = 5
+        view.addSubview(pageControl)
+        pageControl.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+        pageControl.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
+        pageControl.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        pageControl.heightAnchor.constraint(equalToConstant: 50).isActive = true
+
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        scrollView.delegate = self
+        self.view.addSubview(scrollView)
+        scrollView.isPagingEnabled = true
+        scrollView.backgroundColor = .darkGray
+        scrollView.clipsToBounds = false
+        scrollView.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: portfolioWidthToContainingViewFactor).isActive = true
+        scrollView.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: portfolioHeightToContainingViewFactor).isActive = true
+        scrollView.bottomAnchor.constraint(equalTo: pageControl.topAnchor).isActive = true
+        scrollView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+
+        let pannableView = UIView()
+        pannableView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(pannableView)
+        pannableView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+        pannableView.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
+        pannableView.topAnchor.constraint(equalTo: scrollView.topAnchor).isActive = true
+        pannableView.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor).isActive = true
+        pannableView.addGestureRecognizer(scrollView.panGestureRecognizer)
+
+        let contentView = UIView()
+        contentView.translatesAutoresizingMaskIntoConstraints = false
+        scrollView.addSubview(contentView)
+        contentView.leftAnchor.constraint(equalTo: scrollView.leftAnchor).isActive = true
+        contentView.rightAnchor.constraint(equalTo: scrollView.rightAnchor).isActive = true
+        contentView.topAnchor.constraint(equalTo: scrollView.topAnchor).isActive = true
+        contentView.heightAnchor.constraint(equalTo: scrollView.heightAnchor).isActive = true
+        contentView.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor).isActive = true
+
+        let portfolioColors: [UIColor] = [.green, .yellow, .blue, .orange, .red]
+
+        var horizontalAnchor: NSLayoutAnchor = contentView.leftAnchor
+        for x in 0..<5 {
+            let portfolioSpacingContainerView = UIView()
+            portfolioSpacingContainerView.translatesAutoresizingMaskIntoConstraints = false
+            portfolioSpacingContainerView.backgroundColor = .white
+            let portfolioView = UIView()
+            portfolioView.translatesAutoresizingMaskIntoConstraints = false
+            portfolioSpacingContainerView.addSubview(portfolioView)
+            portfolioView.widthAnchor.constraint(equalTo: portfolioSpacingContainerView.widthAnchor, multiplier: 0.94).isActive = true
+            portfolioView.centerXAnchor.constraint(equalTo: portfolioSpacingContainerView.centerXAnchor).isActive = true
+            portfolioView.topAnchor.constraint(equalTo: portfolioSpacingContainerView.topAnchor).isActive = true
+            portfolioView.bottomAnchor.constraint(equalTo: portfolioSpacingContainerView.bottomAnchor).isActive = true
+
+            contentView.addSubview(portfolioSpacingContainerView)
+            portfolioView.backgroundColor = portfolioColors[x]
+            portfolioSpacingContainerView.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: portfolioWidthToContainingViewFactor).isActive = true
+            portfolioSpacingContainerView.heightAnchor.constraint(equalTo: contentView.heightAnchor).isActive = true
+            portfolioSpacingContainerView.leftAnchor.constraint(equalTo: horizontalAnchor).isActive = true
+            portfolioSpacingContainerView.topAnchor.constraint(equalTo: contentView.topAnchor).isActive = true
+            portfolioSpacingContainerView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor).isActive = true
+            horizontalAnchor = portfolioSpacingContainerView.rightAnchor
+        }
+        horizontalAnchor.constraint(equalTo: contentView.rightAnchor).isActive = true
     }
+}
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+extension ViewController: UIScrollViewDelegate {
+
+    func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
+        let pageSize = scrollView.contentSize.width / CGFloat(pageControl.numberOfPages)
+        let currentPage = Int((scrollView.contentOffset.x + pageSize/2) / pageSize)
+        pageControl.currentPage = currentPage
     }
-
-
 }
 
