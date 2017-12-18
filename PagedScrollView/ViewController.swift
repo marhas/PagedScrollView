bc829060d645829e0ba6dee61359d46b9583fd78
Marcel Hasselaar <marcel@unbadapps.com>
2017-12-18 08:49:11 +0100

    Using hit testing to catch events that are normally not delivered to the scrollview to be able to pan from the edges of the screen.

--- PagedScrollView/ViewController.swift
+++ PagedScrollView/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController {
     private let portfolioWidthToContainingViewFactor = CGFloat(0.85)
     private let portfolioHeightToContainingViewFactor = CGFloat(0.6)
 
-    fileprivate let scrollView = UIScrollView()
+    fileprivate var scrollView: UIScrollView!
     fileprivate let pageControl = UIPageControl()
 
     override func viewDidLoad() {
@@ -29,25 +29,25 @@ class ViewController: UIViewController {
         pageControl.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
         pageControl.heightAnchor.constraint(equalToConstant: 50).isActive = true
 
+        let pannableView = UIView()
+        scrollView = EventCatchingScrollView(catchingUIEventsIn: pannableView)
         scrollView.translatesAutoresizingMaskIntoConstraints = false
         scrollView.delegate = self
         self.view.addSubview(scrollView)
         scrollView.isPagingEnabled = true
-        scrollView.backgroundColor = .darkGray
         scrollView.clipsToBounds = false
         scrollView.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: portfolioWidthToContainingViewFactor).isActive = true
         scrollView.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: portfolioHeightToContainingViewFactor).isActive = true
         scrollView.bottomAnchor.constraint(equalTo: pageControl.topAnchor).isActive = true
         scrollView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
 
-        let pannableView = UIView()
         pannableView.translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(pannableView)
         pannableView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
         pannableView.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
         pannableView.topAnchor.constraint(equalTo: scrollView.topAnchor).isActive = true
         pannableView.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor).isActive = true
-        pannableView.addGestureRecognizer(scrollView.panGestureRecognizer)
+        pannableView.isUserInteractionEnabled = false
 
         let portfolioColors: [UIColor] = [.green, .yellow, .blue, .orange, .red]
 
@@ -64,6 +64,15 @@ class ViewController: UIViewController {
             portfolioView.topAnchor.constraint(equalTo: portfolioSpacingContainerView.topAnchor).isActive = true
             portfolioView.bottomAnchor.constraint(equalTo: portfolioSpacingContainerView.bottomAnchor).isActive = true
 
+            let button = UIButton(type: .roundedRect)
+            button.translatesAutoresizingMaskIntoConstraints = false
+            portfolioView.addSubview(button)
+            button.setTitle("I'm a button. Tap me!", for: .normal)
+            button.backgroundColor = portfolioColors[5 - x - 1]
+            button.bottomAnchor.constraint(equalTo: portfolioView.bottomAnchor, constant: -20).isActive = true
+            button.centerXAnchor.constraint(equalTo: portfolioView.centerXAnchor).isActive = true
+            button.addTarget(self, action: #selector(buttonPressed), for: .touchUpInside)
+
             scrollView.addSubview(portfolioSpacingContainerView)
             portfolioView.backgroundColor = portfolioColors[x]
             portfolioSpacingContainerView.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: portfolioWidthToContainingViewFactor).isActive = true
@@ -75,6 +84,11 @@ class ViewController: UIViewController {
         }
         horizontalAnchor.constraint(equalTo: scrollView.rightAnchor).isActive = true
     }
+
+    @objc
+    func buttonPressed(sender: Any) {
+        print("Button pressed: \(sender)")
+    }
 }
 
 extension ViewController: UIScrollViewDelegate {
@@ -83,5 +97,24 @@ extension ViewController: UIScrollViewDelegate {
         let pageSize = scrollView.contentSize.width / CGFloat(pageControl.numberOfPages)
         let currentPage = Int((scrollView.contentOffset.x + pageSize/2) / pageSize)
         pageControl.currentPage = currentPage
+    }
+}
+
+class EventCatchingScrollView: UIScrollView {
+
+    let referenceView: UIView
+
+    init(catchingUIEventsIn view: UIView) {
+        self.referenceView = view
+        super.init(frame: CGRect.zero)
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    override func point(inside point: CGPoint, with event: UIEvent?) -> Bool {
+        let translatedPoint = convert(point, to: referenceView)
+        return referenceView.point(inside: translatedPoint, with: event)
     }
 }
